34ce615db48b1010e7b5e87f92717cd9b4b80e90
WangMingpeng <wmpeng@outlook.com>
2026-05-07 09:20:32 +0800

    untrack scripts/plan-usage-config.local.js

--- scripts/plan-usage-config.local.js
+++
@@ -1,4 +0,0 @@
-var PLAN_USAGE_CONFIG = PLAN_USAGE_CONFIG || {
-    apiBase: 'https://congenial-space-trout-p77qjwpqpw29rvj-5005.app.github.dev'
-    // apiBase: 'https://api.dreamfree.space/relay_detect_dev'
-};
